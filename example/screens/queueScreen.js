da9a497acef353fc0af1ca10f9c4e7e6d7ac1cd8
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-07-14 21:07:53 -0700

    [add] Work on queueScreen

--- example/screens/queueScreen.js
+++ example/screens/queueScreen.js
@@ -1,14 +1,31 @@
 import React, { PureComponent } from "react";
-import { Alert, StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import { View, Text, Image, TextInput, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import Spotify from "rn-spotify-sdk";
 
 export default class QueueScreen extends PureComponent {
   render() {
     return (
-      <View>
-        <Text>You are in the queueScreen</Text>
-        <Text>The code is: {this.props.navigation.state.params.code}</Text>
+      <View style={styles.pageWrapper}>
+        <View style={styles.logoWrapper}>
+          <Image source={require("../assets/images/logo.png")} style={styles.logo} />
+        </View>
+
+        <View>
+          <Text>You are in the queueScreen</Text>
+          <Text>The code is: {this.props.navigation.state.params.code}</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  pageWrapper: {
+    backgroundColor: "#272727",
+    height: "100%",
+  },
+  logoWrapper: {
+    paddingTop: 30,
+    paddingLeft: 30,
+  },
+};
